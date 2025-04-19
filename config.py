a5f9fab65b3bed9c1b5d6bda2140fc05e24ce4f6
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-04-19 16:18:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+# Telegram Bot Token
+BOT_TOKEN = "8120936026:AAE-LYykj7ZEGSxEaAnKq9E_wP38PVo2GJM"
+
+# 5sim API Key for OTP services
+API_KEY = "a4ac091e88004e00ba43894f854a789d"
+
+# BharatPe UPI Payment Information
+UPI_ID = "BHARATPE.8X0M0S6J8F70781@fbpe"
+QR_CODE_LINK = "https://i.ibb.co/BHS157vz/BHARATPE-QR.png"
+ACCESS_TOKEN = "75c8651095404000b35d3135e78d05fe"
+MERCHANT_ID = 53177293
+
+# Admin Telegram ID
+ADMIN_ID = 7459732827
+
+# Support link
+SUPPORT_URL = "https://t.me/akotpshop"
+
+# MongoDB Connection URI
+MONGO_URI = "mongodb://localhost:27017/"
+
+# Emoji Constants for Button UI
+EMOJIS = {
+    "get_otp": "🛒",
+    "recharge": "💳",
+    "profile": "👥",
+    "promo_code": "🎁",
+    "support": "📨",
+    "admin_panel": "🛠️"
+}
