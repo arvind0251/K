f35b0086e04aa79ffcb3b09fb4f6b2fed6fe2519
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-04-20 13:24:12 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,88 +1,46 @@
-from .mongo_client import db
+from pymongo import MongoClient
+from config import MONGO_URI
 
-users = db["users"]
-services = db["services"]
-countries = db["countries"]
-used_utrs = db["used_utrs"]
-promo_codes = db["promo_codes"]  # ✅ Added
+client = MongoClient(MONGO_URI)
+db = client.get_database("k_master")
 
-# -------------------------------
-# USER FUNCTIONS
-# -------------------------------
+users = db.users
+countries = db.countries
+services = db.services
+promos = db.promos
 
-def get_user(chat_id):
-    return users.find_one({"_id": chat_id})
+# USER
+def get_user(chat_id): return users.find_one({"_id": chat_id})
+def create_user(chat_id, data): users.insert_one(data)
+def update_user(chat_id, data): users.update_one({"_id": chat_id}, {"$set": data})
 
-def create_user(chat_id, data):
-    data["_id"] = chat_id
-    users.insert_one(data)
-
-def update_user(chat_id, updates):
-    users.update_one({"_id": chat_id}, {"$set": updates})
-
-# -------------------------------
-# SERVICE FUNCTIONS
-# -------------------------------
-
-def add_service(name, sid, price, cid=None, cname=None):
-    service_data = {
-        "name": name,
-        "id": sid,
-        "price": int(price)
-    }
-    if cid and cname:
-        service_data["country_id"] = cid
-        service_data["country_name"] = cname
-
-    services.update_one(
-        {"name": name, "country_id": cid} if cid else {"name": name},
-        {"$set": service_data},
-        upsert=True
-    )
-
-def get_all_services():
-    return list(services.find())
-
-def get_service_by_name(name):
-    return services.find_one({"name": name})
-
-def get_services_by_country_id(cid):
-    return list(services.find({"country_id": cid}))
-
-# -------------------------------
-# COUNTRY FUNCTIONS
-# -------------------------------
-
+# COUNTRY
 def add_country(name, cid):
-    countries.update_one(
-        {"id": cid},
-        {"$set": {"name": name, "id": cid}},
-        upsert=True
-    )
+    countries.update_one({"id": cid}, {"$set": {"name": name, "id": cid}}, upsert=True)
 
 def get_all_countries():
     return list(countries.find())
 
-# -------------------------------
-# UTR FUNCTIONS (Anti-Reuse)
-# -------------------------------
-
-def is_utr_used(utr):
-    return used_utrs.find_one({"utr": utr}) is not None
-
-def mark_utr_as_used(utr):
-    used_utrs.insert_one({"utr": utr})
-
-# -------------------------------
-# PROMO CODE FUNCTIONS ✅
-# -------------------------------
-
-def add_promo(code, amount):
-    promo_codes.update_one(
-        {"code": code.upper()},
-        {"$set": {"code": code.upper(), "amount": amount}},
+# SERVICE
+def add_service(name, sid, price, country_id, country_name):
+    services.update_one(
+        {"id": sid, "country_id": country_id},
+        {"$set": {
+            "name": name,
+            "id": sid,
+            "price": price,
+            "country_id": country_id,
+            "country_name": country_name
+        }},
         upsert=True
     )
 
+def get_services_by_country_id(country_id):
+    return list(services.find({"country_id": country_id}))
+
+# PROMO
+def add_promo(code, amount):
+    promos.update_one({"code": code.upper()}, {"$set": {"amount": amount}}, upsert=True)
+
 def get_promo(code):
-    return promo_codes.find_one({"code": code.upper()})
+    return promos.find_one({"code": code.upper()})
